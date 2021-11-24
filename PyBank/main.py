e3263e58a1964a08a017beda3ae7a0f480d8e1ab
Daniela <daniela.c.git@gmail.com>
2021-11-24 17:48:06 +1100

    open scv file, count months

---
+++ PyBank/main.py
@@ -0,0 +1,31 @@
+import os
+import csv
+cwd = os.getcwd()
+#print(cwd)
+
+path = os.path.join("Resources","budget_data.csv")
+
+months=0
+monthcrt=0
+monthprev=0
+profits=0
+changes=[]
+
+with open(path, newline="") as csvfile:
+    csvreader=csv.reader(csvfile, delimiter=",")
+    csvheader=next(csvreader)
+    for row in csvreader:
+        months += 1
+        profits += int(row[1])
+        monthcrt += int(row[1])
+        changes.append(monthcrt-monthprev)
+        monthprev=int(row[1])
+print("Financial Analysis")
+print("-------------------") 
+print("Total Months: "+str(months))  
+print("Average Change: $" ) 
+
+
+
+
+    
