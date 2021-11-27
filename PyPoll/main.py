18d22cf78af2e658dac8c5a90a0aacadcb85c2a5
Daniela <daniela.c.git@gmail.com>
2021-11-27 16:52:09 +1100

    Pypoll - format the text file and add comments into the code file

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,11 +5,13 @@
 #	The total number of votes each candidate won
 #	The winner of the election based on popular vote 
 
+# import modules for Python interaction with operating system and comma separated files 
 import os
 import csv
 from typing import List
 pypollpath=os.path.join("Resources","election_data.csv")
 
+#open csv file and calculate total votes each candidate won
 totalvotes=0
 with open(pypollpath, newline="") as csvfile:
     csvreaderp=csv.reader(csvfile, delimiter=",")
@@ -32,11 +34,13 @@ with open(pypollpath, newline="") as csvfile:
         elif row[2] =="O'Tooley":
             Otooleyvotes +=1    
 
+    # calculate percentages of votes each candidate won
     Correyper=format((Correyvotes/totalvotes)*100,".3f")
     Khanper=format((Khanvotes/totalvotes)*100,".3f")
     Liper=format((Livotes/totalvotes)*100,".3f")
     Otooleyper=format((Otooleyvotes/totalvotes)*100,".3f")
 
+    # create a dictionary to match candidates with votes, to be used for finding the winner    
     candidateslist= ["Correy",
     "Khan",
     "Li",
@@ -53,12 +57,13 @@ with open(pypollpath, newline="") as csvfile:
             dictionary[candidate]=votes
             voteslist.remove(votes)
             break
-    
+    # find teh winner
     max=max(dictionary.values())
     for candidate, votes in dictionary.items():
         if votes==max:
             Winner=candidate
-        
+
+# print results to terminal        
 print("Election Results\n")  
 print("-----------------------\n") 
 print("Total Votes:"+str(totalvotes)+"\n") 
@@ -70,17 +75,18 @@ print(f"Otooley: {Otooleyper}% ({Otooleyvotes})\n")
 print(f"Winner: {Winner}")
 print("-----------------------\n") 
 
+# prepare and export a text file with the results
 Votes=os.path.join("Election Results.txt")
 
 with open(Votes,"w") as file:
 
     file.write("Election Results\n")  
     file.write("-----------------------\n") 
-    file.write("Total Votes:"+str(totalvotes)+"\n") 
+    file.write("Total Votes: "+str(totalvotes)+"\n") 
     file.write("-----------------------\n")
-    file.write(f"Khan: {Khanper}% ({Khanvotes}")
-    file.write(f"Coorey: {Correyper}% ({Correyvotes}")
-    file.write(f"Li: {Liper}% ({Livotes})")
-    file.write(f"Otooley: {Otooleyper}% ({Otooleyvotes})\n")
-    file.write(f"Winner: + {Winner}")
+    file.write(f"Khan:    {Khanper}% ({Khanvotes})\n")
+    file.write(f"Coorey:  {Correyper}% ({Correyvotes})\n")
+    file.write(f"Li:      {Liper}% ({Livotes})\n")
+    file.write(f"O'Tooley: {Otooleyper}% ({Otooleyvotes})\n")
+    file.write(f"Winner:  {Winner}\n")
     file.write("-----------------------\n") 
